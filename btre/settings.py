6c713e4b3a85d6c12d60c83b5ab796eb8206107b
Divyang Patel <nextsolutionofficial@gmail.com>
2022-03-15 20:55:42 +0530

    Forget Password minor bug

--- btre/settings.py
+++ btre/settings.py
@@ -161,6 +161,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'subwaywebdesign@gmail.com'
-EMAIL_HOST_PASSWORD = 'koivinahi@9099'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
